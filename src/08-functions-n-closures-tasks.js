b0c6a32bc4cdd76e63dd456c838299b67a7d7f26
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-06-24 20:34:34 +0500

    feat: functions and solutions added solution

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -159,8 +159,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  const el = args1;
+  // eslint-disable-next-line no-shadow
+  return (...args1) => el.join('').toString() + (args1).join('').toString();
 }
 
 
@@ -181,8 +183,10 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let num = startFrom;
+  // eslint-disable-next-line no-plusplus
+  return () => num++;
 }
 
 
